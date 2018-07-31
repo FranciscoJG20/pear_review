716c176e7c5ff19c13a617ff035b6e6647f5391b
Francisco <fjgutierrez112@gmail.com>
2018-07-31 14:37:32 -0400

    set up my environment

---
+++ .env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/franciscogutierrez/wdi/projects/pear_review/pear_review_project/.env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ pear_review_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PearReviewAppConfig(AppConfig):
+    name = 'pear_review_app'
