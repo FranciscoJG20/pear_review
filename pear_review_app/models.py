592ffa713fa2d7a6d2080be861f142335e0d4ef3
Francisco <fjgutierrez112@gmail.com>
2018-07-31 20:38:16 -0400

    defined issue and comment models

--- pear_review_app/models.py
+++ pear_review_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Issue(models.Model):
+    title = models.CharField(max_length=100 , blank=True)
+    body = models.TextField(blank=True)
+    screenshot_url = models.TextField(blank=True)
+    OPEN = 'OPEN'
+    CLOSED = 'CLOSED'
+    ISSUE_STATUS_OPTIONS = (
+        ('OPEN', 'OPEN'),
+        ('CLOSED', 'CLOSED')
+    )
+    issue_status = models.CharField(
+        max_length=6,
+        choices=ISSUE_STATUS_OPTIONS,
+        default = OPEN,
+    )
+
+    def __str__(self):
+        return self.title 
+
+    # ^^^ This method defines what an instance of the model will show up as by default. 
+    # It will be really helpful for debugging in the future!
+
+class Comment(models.Model):
+    content = models.TextField(blank=True)
+    author = models.CharField(max_length=200)
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return self.author  
