7b35e08e766c0b2e666ab226b8bf5388e01adb14
Francisco <fjgutierrez112@gmail.com>
2018-08-01 20:26:46 -0400

    added signup views function

--- pear_review_app/views.py
+++ pear_review_app/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render, redirect
-from .models import Issue, Comment
+from django.contrib.auth.decorators import login_required
 
+from .models import Issue, Comment
 from .forms import IssueForm, CommentForm
 
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+
+
 # Create your views here.
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('issue_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
 
 def issue_list(request):
     issues = Issue.objects.all()
@@ -13,7 +31,7 @@ def issue_detail(request, pk):
     issue = Issue.objects.get(id=pk)
     return render(request, 'pear_review/issue_detail.html', {'issue': issue})    
 
-
+@login_required
 def issue_create(request):
     if request.method == 'POST':
         form = IssueForm(request.POST)
@@ -22,8 +40,9 @@ def issue_create(request):
             return redirect('issue_detail', pk=issue.pk)
     else:
         form = IssueForm()
-    return render(request, 'pear_review/issue_form.html', {'form': form})   
+    return render(request, 'pear_review/issue_form.html', {'form': form})  
 
+@login_required
 def issue_edit(request, pk):
     issue = Issue.objects.get(id=pk)
     if request.method == "POST":
@@ -35,6 +54,7 @@ def issue_edit(request, pk):
         form = IssueForm(instance=issue)
     return render(request, 'pear_review/issue_form.html', {'form': form}) 
 
+@login_required
 def issue_delete(request, pk):
     Issue.objects.get(id=pk).delete()
     return redirect('issue_list')    
@@ -47,6 +67,7 @@ def comment_detail(request, pk):
     comment = Comment.objects.get(id=pk)
     return render(request, 'pear_review/comment_detail.html', {'comment': comment})
 
+@login_required
 def comment_create(request):
     if request.method == 'POST':
         form = CommentForm(request.POST)
@@ -57,6 +78,7 @@ def comment_create(request):
         form = CommentForm()
     return render(request, 'pear_review/comment_form.html', {'form': form})   
 
+@login_required
 def comment_edit(request, pk):
     comment = Comment.objects.get(id=pk)
     if request.method == "POST":
@@ -68,6 +90,7 @@ def comment_edit(request, pk):
         form = CommentForm(instance=comment)
     return render(request, 'pear_review/comment_form.html', {'form': form}) 
 
+@login_required
 def comment_delete(request, pk):
     Comment.objects.get(id=pk).delete()
     return redirect('comment_list')    
