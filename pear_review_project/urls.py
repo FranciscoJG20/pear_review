60caabdd475184932c13f52c1f59a8693d0ad930
Francisco <fjgutierrez112@gmail.com>
2018-08-01 20:26:19 -0400

    added urls for login, logout, and signup

--- pear_review_project/urls.py
+++ pear_review_project/urls.py
@@ -13,14 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth import views as auth_views
+from pear_review_app import views as pear_views
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
+from django.conf.urls import include, url
 
 # ^^^ I am adding an import - include 
 # so that I can include other url files in our main one. 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', include('pear_review_app.urls')),
+    path('', include('pear_review_app.urls')),
+    path('accounts/login/', auth_views.login, name='login'),
+    path('accounts/logout/', auth_views.logout, name='logout'),
+    path('accounts/signup/', pear_views.sign_up, name='signup'),
 ]
