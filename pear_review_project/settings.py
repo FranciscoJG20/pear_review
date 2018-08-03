dbc7ec4bd90add3341e826d58adb1e4846a03727
Francisco <fjgutierrez112@gmail.com>
2018-08-03 12:37:38 -0400

    update requirements with heroku django

--- pear_review_project/settings.py
+++ pear_review_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -147,4 +148,8 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = ''
 ## based on blog post ^^ STATIC_ROOT MUST CONTAIN ABSOLUTE PATH 
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
+django_heroku.settings(locals())
